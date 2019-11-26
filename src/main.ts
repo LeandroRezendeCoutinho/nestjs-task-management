8de01b03f9205c098888f203a790ba0142650ae1
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-11-26 09:12:49 -0300

    add clustering

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cluster from 'cluster';
+import * as os from 'os';
+import { Logger } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length;
+
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on( 'online', worker => {
+    Logger.log( 'Worker ' + worker.process.pid + ' is online.' );
+  });
+  cluster.on( 'exit',  ({ process }, code, signal ) => {
+    Logger.log( 'worker ' + process.pid + ' died.' );
+  });
+} else {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  }
+  bootstrap();
 }
-bootstrap();
