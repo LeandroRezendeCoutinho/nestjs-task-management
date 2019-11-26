8d571ee9deb9da64d68518b5b62f1ee45cf6e5d2
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-11-26 09:14:25 -0300

    keep clustering commented

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cluster from 'cluster';
-import * as os from 'os';
-import { Logger } from '@nestjs/common';
+// import * as cluster from 'cluster';
+// import * as os from 'os';
+// import { Logger } from '@nestjs/common';
 
-if (cluster.isMaster) {
-  const cpuCount = os.cpus().length;
+// if (cluster.isMaster) {
+//   const cpuCount = os.cpus().length;
 
-  for (let i = 0; i < cpuCount; i += 1) {
-    cluster.fork();
-  }
+//   for (let i = 0; i < cpuCount; i += 1) {
+//     cluster.fork();
+//   }
 
-  cluster.on( 'online', worker => {
-    Logger.log( 'Worker ' + worker.process.pid + ' is online.' );
-  });
-  cluster.on( 'exit',  ({ process }, code, signal ) => {
-    Logger.log( 'worker ' + process.pid + ' died.' );
-  });
-} else {
+//   cluster.on( 'online', worker => {
+//     Logger.log( 'Worker ' + worker.process.pid + ' is online.' );
+//   });
+//   cluster.on( 'exit',  ({ process }, code, signal ) => {
+//     Logger.log( 'worker ' + process.pid + ' died.' );
+//   });
+// } else {
   async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
   }
   bootstrap();
-}
+// }
