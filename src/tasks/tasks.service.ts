f383cf0a8e0bfeb257352bf3fd9cb24b0f2478cc
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-11-28 08:10:39 -0300

    get all tasks with repository pattern

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,28 +14,9 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-
-  // getTasksWithFilters(filterDto: GetTaskFilterDTO): Task[] {
-  //   const { status, search } = filterDto;
-
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter(task =>
-  //       task.title.includes(search) ||
-  //       task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
+  async getTasks(filterDto: GetTaskFilterDTO): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
