5fbed8a60b2055502d7aea6ca2af0d2a8b950377
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-12-19 07:52:12 -0300

    change to sqlite just for make it easier

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.HOST || 'localhost',
-  port: 5432,
-  username: process.env.USER_NAME || 'postgres',
-  password: process.env.PASSWORD || 'postgres',
-  database: process.env.DATABASE || 'postgres',
+  type: 'sqlite',
+  // host: process.env.HOST || 'localhost',
+  // port: 5432,
+  // username: process.env.USER_NAME || 'postgres',
+  // password: process.env.PASSWORD || 'postgres',
+  database: process.env.DATABASE || 'task.db',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
